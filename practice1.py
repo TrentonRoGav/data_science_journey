a2098afc411ada9f6a77cc18af637f8b3fb3d23a
TrentonRoGav <122659219+TrentonRoGav@users.noreply.github.com>
2024-12-24 00:46:08 -0600

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,6 @@
+song = "Shape of You"
+artist = "Ed Sheran"
+release_date = 2017
+billboard_weeks = 12
+
+print(f"For the last {billboard_weeks} weeks {song} by Halifax's very own {artist} has topped the Billboards Top 100 list!")
