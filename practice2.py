633e5c430d852c8954f4213982874a91206d2839
TrentonRoGav <122659219+TrentonRoGav@users.noreply.github.com>
2024-12-25 19:22:01 -0600

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,7 @@
+songs = ["Stand By Me", "I'll Be", "I Want It That Way", "Lazy Song", "Stay"]
+
+for song in songs:
+    print(song)
+
+songs.append("Turning Tables")
+print(f"Updated songs list: {songs}")
